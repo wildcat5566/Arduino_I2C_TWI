6494c0265f40fdd6235d88380245024b3fa51e13
wildcat5566 <b01611005@gmail.com>
2018-02-11 10:14:17 +0800

    updated 2018-02-11 3rd

--- i2c_MRX_twi/i2c_MRX_twi.ino
+++ i2c_MRX_twi/i2c_MRX_twi.ino
@@ -1,16 +1,8 @@
 #include "twi_MRX.h"
-#include <inttypes.h>
-#include "Stream.h"
 
 uint8_t rxBuffer[32];
 uint8_t rxBufferIndex = 0;
 uint8_t rxBufferLength = 0;
-
-uint8_t txAddress = 0;
-uint8_t txBuffer[32];
-uint8_t txBufferIndex = 0;
-uint8_t txBufferLength = 0;
-uint8_t transmitting = 0;
 
 void setup() {
   rxBufferIndex = 0;
@@ -25,25 +17,17 @@ void loop() {
   while (rxBufferLength > rxBufferIndex) { // available
     Serial.print(read());
   }
-  delay(10);
+  delay(1);
   Serial.println();
 }
 
 uint8_t requestFrom(uint8_t address, uint8_t quantity, uint32_t iaddress, uint8_t isize, uint8_t sendStop) {
   if (isize > 0) {
-  beginTransmission(address);
-  //endTransmission(false);
   }
   uint8_t read = twi_readFrom(address, rxBuffer, quantity, sendStop); // perform blocking read into buffer
   rxBufferIndex = 0;
   rxBufferLength = read;
   return read;
-}
-
-void beginTransmission(uint8_t address) {
-  transmitting = 1;
-  txAddress = address;                                               // set address of targeted slave
-  txBufferIndex = txBufferLength = 0;
 }
 
 int read(void) {
